8173c1b6d284a4f4432eaae950f68e0b9ecca7c5
Kseniia Kormalova <kormalovak@gmail.com>
2025-09-22 17:27:16 +0000

    directors list

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
   get("/about", { :controller => "misc", :action => "about" })
-  get("/movies", { :controller => "movies", :action => "index"})
-  get("/movies/:path_id", { :controller => "movies", :action => "show"})
+  get("/movies", { :controller => "movies", :action => "index" })
+  get("/movies/:path_id", { :controller => "movies", :action => "show" })
+  get("/directors", { :controller => "directors", :action => "index" })
 end
