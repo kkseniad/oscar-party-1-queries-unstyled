470eead0e4f647a7a85b87b0328efa965dad68e3
Kseniia Kormalova <kormalovak@gmail.com>
2025-09-22 17:34:09 +0000

    director details page

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,7 +1,16 @@
 class DirectorsController < ApplicationController
   def index
     @list_of_directors = Director.all
-    
+
     render({ :template => "director_templates/index" })
   end
+
+  def show
+    the_id = params.fetch("path_id")
+    matching_director = Director.where({ :id => the_id })
+    @the_director = matching_director.at(0)
+
+    render({ :template => "director_templates/show"})
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
   get("/about", { :controller => "misc", :action => "about" })
+
   get("/movies", { :controller => "movies", :action => "index" })
   get("/movies/:path_id", { :controller => "movies", :action => "show" })
+
   get("/directors", { :controller => "directors", :action => "index" })
+  get("/directors/:path_id", { :controller => "directors", :action => "show" })
+
+  
 end
